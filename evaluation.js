5b366b0fe392a9f361ea052b03d30c65918534b1
cywind1 <chingjusttest@gmail.com>
2021-12-13 10:30:02 +0100

    evaluation_v1

---
+++ evaluation.js
@@ -0,0 +1,32 @@
+// find if the target word is in the given string (transcript)
+
+function countScore(transcript,target){
+let score = 0;
+if (transcript.includes(target) {
+	score += 20
+} 
+return score;
+}
+
+// count the number of occurrence of a target word in the given string (transcript)
+     
+    function countOccurrences(transcript,target) {
+        // split the string by spaces in a
+    let a = transcript.split(" ");
+  
+    // search for pattern in a
+    let count = 0;
+    for (let i = 0; i < a.length; i++)
+    {
+    // if match found, increase count
+    if (target==(a[i]))
+        count++;
+    }
+  
+    return count;
+    }
+    // Driver code
+    let transcript = notes;
+    // call target words from Datamuse API
+    let target = "";
+    document.write(countOccurrences(transcript,target));
